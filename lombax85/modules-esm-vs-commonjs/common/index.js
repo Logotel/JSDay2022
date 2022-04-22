316a1221c0aaba790193c89dfa1ff456875eaf73
Fabio Lombardo <lombax85@gmail.com>
2022-04-22 15:35:53 +0200

    cjs - esm

---
+++ lombax85/modules-esm-vs-commonjs/common/index.js
@@ -0,0 +1,5 @@
+const {add, subtract} = require('./common-module') // commonjs
+
+console.log(add(5, 5)) // 10
+console.log(subtract(10, 5)) // 5
+
---
+++ lombax85/modules-esm-vs-commonjs/esm/index.js
@@ -0,0 +1,5 @@
+import {add, subtract} from './esmodule.mjs' // esm
+
+console.log(add(5, 5)) // 10
+console.log(subtract(10, 5)) // 5
+
